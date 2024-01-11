26c4b8fdbd561ec4e16412f7c76921f85a27a436
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2024-01-11 14:27:37 +0300

    Business-Service Layer

--- src/main/java/com/javacourse/project/hibernateAndJpa/Business/ICityService.java
+++ src/main/java/com/javacourse/project/hibernateAndJpa/Business/ICityService.java
@@ -1,13 +1,12 @@
-package com.javacourse.project.hibernateAndJpa.DataAccess;
+package com.javacourse.project.hibernateAndJpa.Business;
 
 import java.util.List;
 
 import com.javacourse.project.hibernateAndJpa.Entities.City;
 
-public interface ICityDal {
+public interface ICityService {
 	List<City> getAll();
 	void add(City city);
 	void update(City city);
 	void delete(City city);
-	
 }
